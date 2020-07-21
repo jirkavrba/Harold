750a370b9ee881e7ea1987b41b4a1f19523427a1
Jiří Vrba <jiri@vrba.dev>
2020-07-21 11:06:50 +0200

    Basic Scala compiler setup

---
+++ src/main/scala/dev/vrba/harold/HaroldApplication.scala
@@ -0,0 +1,8 @@
+package dev.vrba.harold
+
+object HaroldApplication {
+  // The application entry point
+  def main(arguments: Array[String]): Unit = {
+    println("Hide the pain, Harold.")
+  }
+}
