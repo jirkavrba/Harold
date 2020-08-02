935dd58ade5b96a2fa933b2bebeb83a8bdf497a0
Jiří Vrba <jiri@vrba.dev>
2020-08-02 09:29:44 +0200

    Added missing repository annotation

--- src/main/kotlin/dev/vrba/harold/core/permissions/repositories/PermissionEntriesRepository.kt
+++ src/main/kotlin/dev/vrba/harold/core/permissions/repositories/PermissionEntriesRepository.kt
@@ -2,7 +2,9 @@ package dev.vrba.harold.core.permissions.repositories
 
 import dev.vrba.harold.core.permissions.entities.PermissionEntry
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface PermissionEntriesRepository : CrudRepository<PermissionEntry, Long> {
 
     fun findAllByPermissionIn(permissions: List<String>): List<PermissionEntry>
